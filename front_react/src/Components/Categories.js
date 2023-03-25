470f88744c2d51850c19c880910084432ffcc8ac
xxxCarrie-S <c.yufan.s@gmail.com>
2023-03-25 17:23:06 -0400

    change clothing and shoes to dropdown

--- front_react/src/Components/Categories.js
+++ front_react/src/Components/Categories.js
@@ -1,22 +1,39 @@
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/Form';
-import "../CSS/Categories.css"
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import "../CSS/Categories.css";
 
 const Categories = () => {
-    return (
-        <div className='categories'>
-          <Navbar bg="lg" variant="lg">
-            <Nav className="me-auto">
-              <Nav.Link href="#clothes">Clothes</Nav.Link>
-              <Nav.Link href="#shoes">Shoes</Nav.Link>
-            </Nav>
-            <Form className='searchBar'> 
-              <Form.Control placeholder="Search"/>
-            </Form>
-          </Navbar>
-        </div>
-    )
-}
+  return (
+    <div className="categories">
+      <Navbar bg="lg" variant="lg">
+        <Nav className="me-auto">
+          <NavDropdown title="Clothing" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#clothing/men">
+              Men's Clothing
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#clothing/women">
+              Women's Clothing
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#clothing/kids">
+              Kids Clothing
+            </NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Shoes" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#shoes/men">Men's Shoes</NavDropdown.Item>
+            <NavDropdown.Item href="#shoes/women">
+              Women's Shoes
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#shoes/kids">Kids Shoes</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form className="searchBar">
+          <Form.Control placeholder="Search" />
+        </Form>
+      </Navbar>
+    </div>
+  );
+};
 
 export default Categories;
