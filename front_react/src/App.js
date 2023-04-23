f863ed02d5c10632e7b71fe9698c1182ac3e2f7e
xxxCarrie-S <c.yufan.s@gmail.com>
2023-04-23 12:38:35 -0400

    not yet working

--- front_react/src/App.js
+++ front_react/src/App.js
@@ -1,32 +1,37 @@
-import React, {Component} from 'react';
-import ItemList from './Components/ItemList';
-import Navigation from './Components/Navigation';
-import Categories from './Components/Categories';
-import clothes from './Consts/clothes';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './CSS/App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ItemList from "./Components/ItemList";
+import Navigation from "./Components/Navigation";
+import Categories from "./Components/Categories";
+import userProfile from "./Components/userProfile";
+import clothes from "./Consts/clothes";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./CSS/App.css";
 
 class App extends Component {
-
-  constructor(props) { 
+  constructor(props) {
     super(props);
-    this.state = {  
-
-    }
+    this.state = {};
   }
 
   render() {
     return (
       <div className="tc">
-        <Navigation/>  
-        <div className="main">
-          <Categories/>
-          <ItemList items={clothes}/>
-        </div>
+        <Navigation />
+        <Router>
+          <Routes>
+            <Route path="/#personal">
+              {/* <Main> */}
+              <Categories />
+              <ItemList items={clothes} />
+              {/* </Main> */}
+            </Route>
+            <Route path="/#donation">{/* <Main /> */}</Route>
+          </Routes>
+        </Router>
       </div>
     );
   }
 }
 
 export default App;
-
--- front_react/src/Components/Navigation.js
+++ front_react/src/Components/Navigation.js
@@ -1,16 +1,29 @@
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import "../CSS/Navigation.css"
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
+import "../CSS/Navigation.css";
 
-const Navigation = () =>  {
-    return (
-      <Navbar bg="dark" variant="dark" sticky="top">
-        <Navbar.Brand href="#home">Give&Take</Navbar.Brand>
-        <Nav className='ms-auto'>
-          <Nav.Link href="#profile">Profile</Nav.Link>
-        </Nav>
-      </Navbar>
-    )
-}
+const Navigation = () => {
+  return (
+    <Navbar bg="dark" variant="dark" sticky="top">
+      <Navbar.Brand as={Link} to="/">
+        Give&Take
+      </Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link as={Link} to="personal">
+          Personal
+        </Nav.Link>
+        <Nav.Link as={Link} to="donation">
+          Donation
+        </Nav.Link>
+      </Nav>
+      <Nav className="ms-auto">
+        <Nav.Link as={Link} to="profile">
+          Profile
+        </Nav.Link>
+      </Nav>
+    </Navbar>
+  );
+};
 
 export default Navigation;
